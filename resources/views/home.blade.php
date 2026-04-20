dbcb6c03635e9af36dcba1691aba004c147cda9a
Prassxcc <241230048@unmuhpnk.ac.id>
2026-04-20 15:48:21 +0700

    membuat view dan controller

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <a href="/weblanjut">...</a>
+    <a href="/">Home</a>
+    <a href="/">Profile</a>
+    <a href="/">Kontak</a>
+
+    <div class="p"><h1>halaman Home</h1></div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\homeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [homeController::class, 'index']);
